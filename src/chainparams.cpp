ee6f46211177d3b37444e34cf01e3fc19cbb6bf7
shuurai <hyperfan@hotmail.com>
2021-02-08 14:21:20 +0800

    Updated checkpoint

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -161,8 +161,8 @@ public:
             {
                 {       0, uint256S("5fc6259da4a120a30e14fe535dcbe427cc947feb516d4d786e926ef6cf6a1cb8")}.
                 {     746, uint256S("0a3600dc8998f09a8e1df60705877b3a0eb95776c4622bd94aea1f0527ca49b1")},
-                {    3268, uint256S("52d7d62d4bf82aea25051b763132dce5072f7de8592191e7a2ee43995a27798a")}
-                // {  8064, uint256S("0xeb984353fc5190f210651f150c40b8a4bab9eeeff0b729fcb3987da694430d70")},
+                {    3268, uint256S("52d7d62d4bf82aea25051b763132dce5072f7de8592191e7a2ee43995a27798a")},
+                {  4560, uint256S("8d87150b01f5e44ecf058458c56f11c4d9c13f92be7fe449fba290c6b6cfdff6")}
                 // { 16128, uint256S("0x602edf1859b7f9a6af809f1d9b0e6cb66fdc1d4d9dcd7a4bec03e12a1ccd153d")},
                 // { 23420, uint256S("0xd80fdf9ca81afd0bd2b2a90ac3a9fe547da58f2530ec874e978fce0b5101b507")},
                 // { 50000, uint256S("0x69dc37eb029b68f075a5012dcc0419c127672adb4f3a32882b2b3e71d07a20a6")},
