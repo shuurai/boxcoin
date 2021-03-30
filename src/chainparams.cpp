e5ea79aaf87ea356f9c196f41c4255e8fdd47066
Shane Fan Di <shane.fdi@gmail.com>
2021-03-30 13:50:12 +0800

    Updated dedicated test net for the future

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -232,7 +232,10 @@ public:
         vFixedSeeds.clear();
         vSeeds.clear();
         // nodes with support for servicebits filtering should be at the top
-        vSeeds.emplace_back("test0001sg.walklife.io");
+        vSeeds.emplace_back("test0001sg.walklife.io"); // dedicated test net for the future
+        vSeeds.emplace_back("s0002sg.walklife.io");
+        vSeeds.emplace_back("s0003sg.walklife.io");
+        vSeeds.emplace_back("s0001ny.walklife.io");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
